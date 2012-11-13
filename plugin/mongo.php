ea5afd883d1c058d753713d1fd7925e05271200f
Poil <poil@quake.fr>
2012-11-13 14:27:18 +0100

    Fix mongo plugin

--- plugin/mongo.php
+++ plugin/mongo.php
@@ -16,77 +16,58 @@ require_once 'modules/collectd.inc.php';
 # apcups/voltage-output.rrd
 
 $obj = new Type_Default($CONFIG);
-$obj->width = $width;
-$obj->heigth = $heigth;
 
 switch($obj->args['type']) {
-	case 'cache_ratio':
-		$obj->data_sources = array('value');
-                $obj->rrd_title = sprintf('Cache Ratio%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = 'Hits';
-	break;
-	case 'connections':
-		$obj->data_sources = array('value');
-		$obj->rrd_title = sprintf('Connections%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = 'Numbers';
-	break;
-	case 'memory':
-                require_once 'type/GenericStacked.class.php';
-                $obj = new Type_GenericStacked($CONFIG);
-                $obj->width = $width;
-                $obj->heigth = $heigth;
+	case 'cache_ratio' :
+		//$obj -> data_sources = array('value');
+		$obj -> rrd_title = sprintf('Cache Ratio%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'Hits';
+		break;
+	case 'connections' :
+		//$obj -> data_sources = array('value');
+		$obj -> rrd_title = sprintf('Connections%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'Numbers';
+		break;
+	case 'memory' :
+		require_once 'type/GenericStacked.class.php';
+		$obj = new Type_GenericStacked($CONFIG);
+		$obj -> width = $width;
+		$obj -> heigth = $heigth;
 
-		$obj->data_sources = array('value');
-		$obj->rrd_title = sprintf('Memory%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = 'MB';
-	break;
-	case 'counter':
-                require_once 'type/GenericStacked.class.php';
-                $obj = new Type_GenericStacked($CONFIG);
-                $obj->width = $width;
-                $obj->heigth = $heigth;
-		$obj->data_sources = array('value');
-		$obj->rrd_title = sprintf('Counter%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = 'Number';
-	break;
-	case 'percent':
-		$obj->data_sources = array('value');
-		$obj->rrd_title = sprintf('Lock Ratio Time%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = '%';
-	break;
-	case 'file_size':
-                require_once 'type/GenericStacked.class.php';
-                $obj = new Type_GenericStacked($CONFIG);
-                $obj->width = $width;
-                $obj->heigth = $heigth;
-		$obj->data_sources = array('bytes');
-		$obj->rrd_title = sprintf('File Size%s',
-                        !empty($obj->args['pinstance']) ? ' ('.$obj->args['pinstance'].')' : '');
-		$obj->rrd_vertical = 'Bytes';
-	break;
+		//$obj -> data_sources = array('value');
+		$obj -> rrd_title = sprintf('Memory%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'MB';
+		break;
+	case 'counter' :
+		require_once 'type/GenericStacked.class.php';
+		$obj = new Type_GenericStacked($CONFIG);
+		//$obj -> data_sources = array('value');
+		$obj -> rrd_title = sprintf('Counter%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'Number';
+		break;
+	case 'percent' :
+		//$obj -> data_sources = array('value');
+		$obj -> rrd_title = sprintf('Lock Ratio Time%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = '%';
+		break;
+	case 'file_size' :
+		require_once 'type/GenericStacked.class.php';
+		$obj = new Type_GenericStacked($CONFIG);
+		//$obj -> data_sources = array('bytes');
+		$obj -> rrd_title = sprintf('File Size%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'Bytes';
+		break;
+	case 'total_operations' :
+		require_once 'type/GenericStacked.class.php';
+		$obj = new Type_GenericStacked($CONFIG);		
+		$obj -> rrd_title = sprintf('Total Operation%s', !empty($obj -> args['pinstance']) ? ' (' . $obj -> args['pinstance'] . ')' : '');
+		$obj -> rrd_vertical = 'Operation';
 }
-$obj->rrd_format = '%5.1lf%s';
-$obj->generate_colors();
+$obj -> width = $width;
+$obj -> heigth = $heigth;
+$obj -> rrd_format = '%5.1lf%s';
+$obj -> generate_colors();
 
-
-# backwards compatibility
-if ($CONFIG['version'] < 5 &&
-	in_array($obj->args['type'], array('frequency', 'percent', 'timeleft'))) {
-
-	$obj->data_sources = array($obj->args['type']);
-
-	$obj->ds_names[$obj->args['type']] = $obj->ds_names['value'];
-	unset($obj->ds_names['value']);
-
-	$obj->colors[$obj->args['type']] = $obj->colors['value'];
-	unset($obj->colors['value']);
-}
-
-collectd_flush($obj->identifiers);
-$obj->rrd_graph();
+collectd_flush($obj -> identifiers);
+$obj -> rrd_graph();
 ?>
