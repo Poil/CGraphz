76e0eb6917b22f882b04b3d1e876440edf402848
Poil <poil@quake.fr>
2013-01-25 10:35:10 +0100

    Little fix

--- lib/dyn_js.php
+++ lib/dyn_js.php
@@ -9,3 +9,12 @@ $(function(){
        	$.localise('ui.multiselect', {language: '<?php echo DEF_LANG ?>',  path: 'lib/multiselect/locale/'});
        	$(".multiselect").multiselect();
 });
+
+$(document).ready(function() {
+   $('.table_admin').dataTable( {
+      "bStateSave": true,
+      "oLanguage": {
+         "sUrl": "lib/dataTables/locale/ui.dataTables-<?php echo DEF_LANG ?>.js"
+      }
+   });
+});
