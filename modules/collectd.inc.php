132924e8b15bf55af5a17457a2ed452b18603d2c
Poil <poil@quake.fr>
2013-03-10 21:21:10 +0100

    Try to have rrdflush and collectdflush depending of configuration

--- modules/collectd.inc.php
+++ modules/collectd.inc.php
@@ -215,21 +215,48 @@ function collectd_flush($identifier) {
 	if (!$CONFIG['socket'])
 		return FALSE;
 
+	if (!$identifier || (is_array($identifier) && count($identifier) == 0) || !(is_string($identifier) || is_array($identifier)))
+		return FALSE;
+
 	$u_errno  = 0;
 	$u_errmsg = '';
 	if ($socket = @fsockopen($CONFIG['socket'], 0, $u_errno, $u_errmsg)) {
-		if (!$identifier || (is_array($identifier) && count($identifier) == 0) ||
-			!(is_string($identifier) || is_array($identifier)))
-		return FALSE;
-
-		if (is_array($identifier)) {
-			foreach ($identifier as $val) {
-				$cmd = 'FLUSH '.$val.'.rrd'."\n";
+		if ($CONFIG['flush_type'] == 'rrd') {
+			if (is_array($identifier)) {
+				foreach ($identifier as $val) {
+					$cmd = 'FLUSH '.$val.'.rrd'."\n";
+					rrdcached_flush($socket, $cmd);
+				}
+			} else {
+				$cmd = 'FLUSH '.$identifier.'.rrd'."\n";
 				rrdcached_flush($socket, $cmd);
 			}
-		} else {
-			$cmd = 'FLUSH '.$identifier.'.rrd'."\n";
-			rrdcached_flush($socket, $cmd);
+		} else if ($CONFIG['flush_type'] == 'collectd'){
+			if (is_array($identifier)) {
+				foreach ($identifier as $val)
+					$cmd .= sprintf(' identifier="%s"', $val);
+			} else {
+				$cmd .= sprintf(' identifier="%s"', $identifier);
+			}
+			$cmd .= "\n";
+				
+			$r = fwrite($socket, $cmd, strlen($cmd));
+			if ($r === false || $r != strlen($cmd)) {
+				error_log(sprintf('ERROR: Failed to write full command to unix-socket: %d out of %d written',
+				$r === false ? -1 : $r, strlen($cmd)));
+				return FALSE;
+			}
+				
+			$resp = fgets($socket);
+			if ($resp === false) {
+				error_log(sprintf('ERROR: Failed to read response from collectd for command: %s',
+				trim($cmd)));
+				return FALSE;
+			}
+				
+			$n = (int)$resp;
+			while ($n-- > 0)
+				fgets($socket);
 		}
 
 		fclose($socket);
