091fa7ca3960ea932f0c5a3964874dd236309e53
Poil <poil@quake.fr>
2013-03-13 16:37:46 +0100

    Missing graph when TCategory

--- html/dashboard/dynamic/r_dynamic.php
+++ html/dashboard/dynamic/r_dynamic.php
@@ -125,7 +125,7 @@ if ($_GET['f_id_config_dynamic_dashboard']) {
 						if (isset($matches[5]) && $matches[5]!=null) {
 							$plugin_array[$cpt_p]['ti']=$matches[5];
 							$plugin_array[$cpt_p]['tc']=null;
-							if (substr_count($plugin_array[$cpt_p]['ti'], '-') >= 1  && preg_match('/^(GenericJMX)$/', $$plugin_array[$cpt_p]['p'])) {
+							if (substr_count($plugin_array[$cpt_p]['ti'], '-') >= 1) {
 								$tmp=explode('-',$plugin_array[$cpt_p]['ti']);
 								$plugin_array[$cpt_p]['tc']=$tmp[0];
 								$plugin_array[$cpt_p]['ti']=implode('-',array_slice($tmp,1));
