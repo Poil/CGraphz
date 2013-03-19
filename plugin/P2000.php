c1da27979c76a3107a39e7aee839e47b99b0c7d6
root <root@pt-bdupuis.esys.local>
2013-03-19 11:24:03 +0100

    P2000 plugin

---
+++ plugin/P2000.php
@@ -0,0 +1,20 @@
+<?php
+
+# Collectd bind plugin
+
+
+require_once 'type/GenericStacked.class.php';
+require_once 'modules/collectd.inc.php';
+
+
+$obj = new Type_GenericStacked($CONFIG);
+$obj->width = $width;
+$obj->heigth = $heigth;
+
+$obj->rrd_title = sprintf('%s/%s-%s', $obj->args['pinstance'], $obj->args['type'], $obj->args['tcategory']);
+$obj->rrd_format = '%5.1lf%s';
+
+$obj->rrd_vertical = str_replace('_',' ',$obj->args['type']);
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
