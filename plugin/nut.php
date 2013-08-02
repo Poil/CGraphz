2cc28430dd922ceca1fa4f13eb916e58ca577683
Poil <poil@quake.fr>
2013-08-02 19:24:02 +0200

    plugin/nut: collectd 5 compatibility for frequency graph

--- plugin/nut.php
+++ plugin/nut.php
@@ -15,37 +15,56 @@ require_once 'modules/collectd.inc.php';
 
 $obj = new Type_Default($CONFIG);
 switch($obj->args['type']) {
-	case 'percent':
-		$obj->data_sources = array('percent');
-		$obj->ds_names = array('charge' => 'Charge',
-		                       'load' => 'Load');
-		$obj->rrd_title = sprintf('Charge & load (%s)', $obj->args['pinstance']);
-		$obj->rrd_vertical = '%';
-		$obj->rrd_format = '%5.1lf';
-	break;
-	case 'temperature':
-		$obj->data_sources = array('value');
-		$obj->ds_names = array('value' => 'Temperature');
-		$obj->rrd_title = sprintf('Temperature (%s)', $obj->args['pinstance']);
-		$obj->rrd_vertical = '°C';
+	case 'frequency':
+		if ($CONFIG['version'] < 5) {
+			$obj->data_sources = array('frequency');
+		} else {
+			$obj->data_sources = array('value');
+		}
+		$obj->ds_names = array('output' => 'Output');
+		$obj->rrd_title = sprintf('Frequency (%s)', $obj->args['pinstance']);
+		$obj->rrd_vertical = 'Hz';
 		$obj->rrd_format = '%5.1lf%s';
-	break;
-	case 'timeleft':
-		$obj->data_sources = array('timeleft');
-		$obj->ds_names = array('timeleft' => 'Timeleft');
-		$obj->rrd_title = sprintf('Timeleft (%s)', $obj->args['pinstance']);
-		$obj->rrd_vertical = 'Seconds';
-		$obj->rrd_format = '%5.1lf';
-	break;
-	case 'voltage':
-		$obj->data_sources = array('value');
-		$obj->ds_names = array('battery' => 'Battery',
-		                       'input' => 'Input',
-		                       'output' => 'Output');
-		$obj->rrd_title = sprintf('Voltage (%s)', $obj->args['pinstance']);
-		$obj->rrd_vertical = 'Volts';
-		$obj->rrd_format = '%5.1lf';
-	break;
+		break;
+		case 'percent':
+			if ($CONFIG['version'] < 5) {
+				$obj->data_sources = array('percent');
+			} else {
+				$obj->data_sources = array('value');
+			}
+			$obj->ds_names = array('charge' => 'Charge',
+						'load' => 'Load');
+			$obj->rrd_title = sprintf('Charge & load (%s)', $obj->args['pinstance']);
+			$obj->rrd_vertical = '%';
+			$obj->rrd_format = '%5.1lf';
+		break;
+		case 'temperature':
+			$obj->data_sources = array('value');
+			$obj->ds_names = array('battery' => 'Battery');
+			$obj->rrd_title = sprintf('Temperature (%s)', $obj->args['pinstance']);
+			$obj->rrd_vertical = '°C';
+			$obj->rrd_format = '%5.1lf%s';
+		break;
+		case 'timeleft':
+			if ($CONFIG['version'] < 5) {
+				$obj->data_sources = array('timeleft');
+			} else {
+				$obj->data_sources = array('value');
+			}
+			$obj->ds_names = array('timeleft' => 'Timeleft');
+			$obj->rrd_title = sprintf('Timeleft (%s)', $obj->args['pinstance']);
+			$obj->rrd_vertical = 'Seconds';
+			$obj->rrd_format = '%5.1lf';
+		break;
+		case 'voltage':
+			$obj->data_sources = array('value');
+			$obj->ds_names = array('battery' => 'Battery',
+						'input' => 'Input',
+						'output' => 'Output');
+			$obj->rrd_title = sprintf('Voltage (%s)', $obj->args['pinstance']);
+			$obj->rrd_vertical = 'Volts';
+			$obj->rrd_format = '%5.1lf';
+		break;
 }
 
 collectd_flush($obj->identifiers);
