b9f9bde41c3709f862323f3ec85e7377f539a27b
Poil <poil@quake.fr>
2013-12-04 20:29:19 +0100

    Fix a DDOS when a user attemped to request a huge image, thus consuming all memory on the host.

--- graph.php
+++ graph.php
@@ -21,40 +21,45 @@ if ($auth->verif_auth()) {
 	$width = empty($_GET['x']) ? $CONFIG['width'] : $_GET['x'];
 	$height = empty($_GET['y']) ? $CONFIG['height'] : $_GET['y'];
 	$s=intval($_GET['s']);
+	if (($width * $height) <= MAX_IMG_SIZE) {	
+		if (validate_get(GET('h'), 'host') === NULL) {
+		        error_log('CGRAPHZ ERROR: plugin contains unknown characters');
+		        error_image();
+		}
+		
+		if (!file_exists(DIR_FSROOT.'/plugin/'.$plugin.'.php')) {
+		        error_log(sprintf('CGRAPHZ ERROR: plugin "%s" is not available', $plugin));
+		        error_image();
+		}
 	
-	if (validate_get(GET('h'), 'host') === NULL) {
-	        error_log('CGRAPHZ ERROR: plugin contains unknown characters');
-	        error_image();
-	}
-	
-	if (!file_exists(DIR_FSROOT.'/plugin/'.$plugin.'.php')) {
-	        error_log(sprintf('CGRAPHZ ERROR: plugin "%s" is not available', $plugin));
-	        error_image();
-	}
+		$lib='
+		SELECT cs.server_name
+		FROM config_server cs
+		  LEFT JOIN config_server_project csp 
+	        ON cs.id_config_server=csp.id_config_server
+		  LEFT JOIN perm_project_group ppg 
+	        ON csp.id_config_project=ppg.id_config_project
+		  LEFT JOIN auth_user_group aug 
+	        ON ppg.id_auth_group=aug.id_auth_group
+		WHERE ( cs.server_name=:host )
+		  AND ( aug.id_auth_user=:user )
+		GROUP BY server_name
+		ORDER BY server_name';
 
-	$lib='
-	SELECT cs.server_name
-	FROM config_server cs
-	  LEFT JOIN config_server_project csp 
-        ON cs.id_config_server=csp.id_config_server
-	  LEFT JOIN perm_project_group ppg 
-        ON csp.id_config_project=ppg.id_config_project
-	  LEFT JOIN auth_user_group aug 
-        ON ppg.id_auth_group=aug.id_auth_group
-	WHERE ( cs.server_name=:host )
-	  AND ( aug.id_auth_user=:user )
-	GROUP BY server_name
-	ORDER BY server_name';
-    $stmt = $connSQL->prepare($lib);
-    $stmt->bindValue(':host', $connSQL->escape($host));
-    $stmt->bindValue(':user',$connSQL->escape(intval($_SESSION['S_ID_USER'])));
-    $stmt->execute();
-	$authorized=$stmt->fetchObject();
-	
-	if ($host==$authorized->server_name) {
-		# load plugin
-		include DIR_FSROOT.'/plugin/'.$plugin.'.php';
-	} else {		
+		$stmt = $connSQL->prepare($lib);
+		$stmt->bindValue(':host', $connSQL->escape($host));
+		$stmt->bindValue(':user',$connSQL->escape(intval($_SESSION['S_ID_USER'])));
+		$stmt->execute();
+		$authorized=$stmt->fetchObject();
+		
+		if ($host==$authorized->server_name) {
+			# load plugin
+			include DIR_FSROOT.'/plugin/'.$plugin.'.php';
+		} else {		
+			error_image();
+		}
+	} else {
+	        error_log('CGRAPHZ ERROR: image request is too big');
 		error_image();
 	}
 }
