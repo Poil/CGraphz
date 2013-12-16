5b6a022e5d87dce0e56d16d2e58aa6e5068e96ad
Poil <poil@quake.fr>
2013-12-16 19:50:31 +0100

    Stupid bug on mydynamic_dashboard

--- html/small_admin/mydashboard_content/r_dynamic_dashboard_content.php
+++ html/small_admin/mydashboard_content/r_dynamic_dashboard_content.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_GET['f_id_config_dynamic_dashboard']) && $cur_dynamic_dashboard) {
-	$f_id_config_dynamic_dashboard_content=filter_input(INPUT_GET,'f_id_config_dynamic_dashboard_content',FILTER_SANITIZE_NUMBER_INT);
+	$f_id_config_dynamic_dashboard=filter_input(INPUT_GET,'f_id_config_dynamic_dashboard',FILTER_SANITIZE_NUMBER_INT);
 
 	$connSQL=new DB();
+	$connSQL->bind('f_id_config_dynamic_dashboard',$f_id_config_dynamic_dashboard);
 	$all_dynamic_dashboard_content=$connSQL->query('SELECT * FROM config_dynamic_dashboard_content 
 		WHERE id_config_dynamic_dashboard=:f_id_config_dynamic_dashboard ORDER BY dash_ordering');
 	$cpt_dynamic_dashboard_content=count($all_dynamic_dashboard_content);
 
 	$connSQL=new DB();
-	$connSQL->bind('f_id_config_dynamic_dashboard_content',$f_id_config_dynamic_dashboard_content);
 	$all_plugin_filter=$connSQL->query('SELECT * FROM config_plugin_filter ORDER BY plugin_order, plugin_filter_desc');
 	$cpt_plugin_filter=count($all_plugin_filter);
 
--- html/small_admin/mydashboard_content/w_dynamic_dashboard_content.php
+++ html/small_admin/mydashboard_content/w_dynamic_dashboard_content.php
@@ -1,19 +1,18 @@
 <?php
 if (isset($_POST['f_submit_dynamic_dashboard_content']) && $cur_dynamic_dashboard) {
-	$f_id_config_dynamic_dashboard=filter_input(INPUT_POST,'f_id_config_dynamic_dashboard',FILTER_SANITIZE_NUMBER_INT);
+	$f_id_config_dynamic_dashboard=filter_input(INPUT_GET,'f_id_config_dynamic_dashboard',FILTER_SANITIZE_NUMBER_INT);
 	$f_id_config_dynamic_dashboard_content=filter_input(INPUT_POST,'f_id_config_dynamic_dashboard_content',FILTER_SANITIZE_NUMBER_INT);
 	$f_dash_ordering=filter_input(INPUT_POST,'f_dash_ordering',FILTER_SANITIZE_NUMBER_INT);
 	$f_title=filter_input(INPUT_POST,'f_title',FILTER_SANITIZE_SPECIAL_CHARS);
-	
 	$f_regex_srv=filter_input(INPUT_POST,'f_regex_srv',FILTER_SANITIZE_SPECIAL_CHARS);
 	$f_regex_p_filter=filter_input(INPUT_POST,'f_regex_p_filter',FILTER_SANITIZE_SPECIAL_CHARS);
 	$f_regex_pi_filter=filter_input(INPUT_POST,'f_regex_pi_filter',FILTER_SANITIZE_SPECIAL_CHARS);
 	$f_regex_t_filter=filter_input(INPUT_POST,'f_regex_t_filter',FILTER_SANITIZE_SPECIAL_CHARS);
 	$f_regex_ti_filter=filter_input(INPUT_POST,'f_regex_ti_filter',FILTER_SANITIZE_SPECIAL_CHARS);
-	
 	$f_rrd_ordering=filter_input(INPUT_POST,'f_rrd_ordering',FILTER_SANITIZE_SPECIAL_CHARS);
 	
-	if ($f_id_config_dynamic_dashboard_content !== 0) {
+	if (!empty($f_id_config_dynamic_dashboard_content)) {
+		$connSQL=new DB();
 		$connSQL->bind('f_id_config_dynamic_dashboard_content',$f_id_config_dynamic_dashboard_content);
 		$lib='
 			UPDATE config_dynamic_dashboard_content SET
@@ -28,7 +27,7 @@ if (isset($_POST['f_submit_dynamic_dashboard_content']) && $cur_dynamic_dashboar
 			WHERE
 				id_config_dynamic_dashboard_content=:f_id_config_dynamic_dashboard_content';
 	} else { // INSERT
-		$connSQL->bind('f_id_config_dynamic_dashboard',$f_id_config_dynamic_dashboard);
+		$connSQL=new DB();
 		$lib='INSERT INTO config_dynamic_dashboard_content 
 			(id_config_dynamic_dashboard, title, regex_srv, regex_p_filter, regex_pi_filter, regex_t_filter, regex_ti_filter, rrd_ordering, dash_ordering) 
 		VALUES 
@@ -36,8 +35,8 @@ if (isset($_POST['f_submit_dynamic_dashboard_content']) && $cur_dynamic_dashboar
 			:f_regex_srv, :f_regex_p_filter, :f_regex_pi_filter,
 			:f_regex_t_filter, :f_regex_ti_filter,
 			:f_rrd_ordering, :f_dash_ordering)';
+		$connSQL->bind('f_id_config_dynamic_dashboard',$f_id_config_dynamic_dashboard);
 	}
-	$connSQL=new DB();
 	$connSQL->bind('f_title',$f_title);
 	$connSQL->bind('f_regex_srv',$f_regex_srv);
 	$connSQL->bind('f_regex_p_filter',$f_regex_p_filter);
