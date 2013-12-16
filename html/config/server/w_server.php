c8ad9d332b12cdc20be82fe14ee3074d1ed4d473
Poil <poil@quake.fr>
2013-12-16 18:10:14 +0100

    Remove not-need stuff

--- html/config/server/w_server.php
+++ html/config/server/w_server.php
@@ -2,11 +2,9 @@
 if (isset($_POST['f_submit_server'])) {
 	$f_id_config_server=filter_input(INPUT_POST,'f_id_config_server',FILTER_SANITIZE_NUMBER_INT);
 	$f_server_description=filter_input(INPUT_POST,'f_server_description',FILTER_SANITIZE_SPECIAL_CHARS);
-	$f_collectd_version=filter_input(INPUT_POST,'f_collectd_version',FILTER_SANITIZE_SPECIAL_CHARS);
+	$f_collectd_version=filter_input(INPUT_POST,'f_collectd_version',FILTER_SANITIZE_NUMBER_FLOAT);
 
 	if ($_POST['f_id_config_server']) { // UPDATE
-		$f_server_name=filter_input(INPUT_POST,'f_server_name',FILTER_SANITIZE_SPECIAL_CHARS);
-
 		$connSQL=new DB();
 		$connSQL->bind('f_server_description',$f_server_description);
 		$connSQL->bind('f_collectd_version',$f_collectd_version);
