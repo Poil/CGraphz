8bd8fee4e823a1ee89b88c00797f1bab73f44d6c
Poil <poil@quake.fr>
2013-12-20 09:01:33 +0100

    Code improvement

--- html/config/server/r_server.php
+++ html/config/server/r_server.php
@@ -4,7 +4,7 @@ $all_server=$connSQL->query('SELECT * FROM config_server ORDER BY server_name');
 $cpt_server=count($all_server);
 
 /* Listing des serveurs présent dans le RRD DIR et pas déjà affectés */
-$filelist=scandir($CONFIG['datadir']);
+$filelist=array_values(array_diff(scandir($CONFIG['datadir']), array('..', '.', 'lost+found')));
 
 $lib='
 CREATE TEMPORARY TABLE server_list (
@@ -17,7 +17,7 @@ $find='0';
 $lib= 'INSERT INTO server_list (server_name) VALUES (';  
 $cpt_filelist=count($filelist);
 for($i=0; $i<$cpt_filelist; $i++) {
-	if ($filelist[$i]!='lost+found' && $filelist[$i]!='..' && $filelist[$i]!='.' && strpos($filelist[$i],':')==false) {
+	if (strpos($filelist[$i],':')==false && is_dir($CONFIG['datadir'].'/'.$filelist[$i])) {
 		if($find=='1')  {
 			$lib.=" ), (";
 		}  
