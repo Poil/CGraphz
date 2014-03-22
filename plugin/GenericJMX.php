b15d30c5588539417c405b4d928e0e0b25f57092
root <root@afm-don1.adm.fr.clara.net>
2014-03-22 19:27:08 +0100

    Fix GenericJMX

--- plugin/GenericJMX.php
+++ plugin/GenericJMX.php
@@ -8,14 +8,12 @@ require_once 'modules/collectd.inc.php';
 $obj = new Type_Default($CONFIG);
 
 if (strlen($obj->args['tcategory'])) {
-	$obj->rrd_title = ucfirst(str_replace('_', ' ', $obj->args['tcategory']));
+	$default_title = ucfirst(str_replace('_', ' ', $obj->args['tcategory'])).' '.str_replace('_', ' ', $obj->args['type']);
 } else {
-	$obj->rrd_title = ucfirst(str_replace('_', ' ', (empty($obj->args['pinstance'])?$obj->args['pcategory']:$obj->args['pinstance'])));
+	$default_title = ucfirst(str_replace('_', ' ', $obj->args['type']));
 }
-$obj->rrd_vertical = ucfirst(str_replace('_', ' ', $obj->args['type']));
-$obj->rrd_format = '%5.1lf%s'; ;;
 
-switch($obj->args['pinstance']) {
+switch($obj->args['pcategory']) {
 	case ((preg_match('/catalina_request_processor/', $obj->args['pinstance']) ? true : false) || preg_match('/catalina_request_processor/', $obj->args['pcategory']) ? true : false) :
 		switch ($obj->args['type']) {
 			case 'io_octets' :
@@ -23,16 +21,20 @@ switch($obj->args['pinstance']) {
 				$obj->data_sources = array('rx', 'tx');
 				$obj->ds_names = array('rx' => 'Receive', 'tx' => 'Transmit', );
 				$obj->colors = array('rx' => '0000ff', 'tx' => '00b000', );
-				$obj->rrd_title = sprintf('Interface Traffic (%s)', str_replace('_', ' ', (empty($obj->args['pinstance'])?$obj->args['pcategory']:$obj->args['pinstance'])));
+				$obj->rrd_title = sprintf('Traffic (%s)', str_replace('_', ' ', (empty($obj->args['pinstance'])?$obj->args['pcategory']:$obj->args['pinstance'])));
 				$obj->rrd_vertical = 'Bytes/s';
 			break;
 			default:
-				error_image('Unknown graph type :'.PHP_EOL.str_replace('&',PHP_EOL,$_SERVER['QUERY_STRING']));
+				$obj->rrd_title = $default_title;
+				$obj->rrd_vertical = sprintf('%s', str_replace('_', ' ', $obj->args['type']));
+				$obj->rrd_format = '%5.1lf%s';
 			break;
 		}
 	break;
 	default:
-		error_image('Unknown graph type :'.PHP_EOL.str_replace('&',PHP_EOL,$_SERVER['QUERY_STRING']));
+		$obj->rrd_title = $default_title;
+		$obj->rrd_vertical = sprintf('%s', str_replace('_', ' ', $obj->args['type']));
+		$obj->rrd_format = '%5.1lf%s';
 	break;
 }
 
