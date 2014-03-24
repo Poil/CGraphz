191ec9c70635f07f229414a329c7a05a813f427d
Poil <poil@quake.fr>
2014-03-24 20:01:42 +0100

    Add module search to perms

--- sql/upgrade_from_2.10_to_2_20.sql
+++ sql/upgrade_from_2.10_to_2_20.sql
@@ -2,3 +2,6 @@ ALTER TABLE `cgraphz`.`config_server` MODIFY collectd_version VARCHAR(16) NULL;
 
 INSERT INTO `cgraphz`.`perm_module` (`id_perm_module`, `module`, `component`, `menu_name`, `menu_order`) VALUES (16, 'dashboard', 'light', NULL, 0);
 INSERT INTO `cgraphz`.`perm_module_group` (`id_perm_module_group`, `id_auth_group`, `id_perm_module`) VALUES (16, 1, 16);
+
+INSERT INTO `cgraphz`.`perm_module` (`id_perm_module`, `module`, `component`, `menu_name`, `menu_order`) VALUES (17, 'dashboard', 'search', NULL, 0);
+INSERT INTO `cgraphz`.`perm_module_group` (`id_perm_module_group`, `id_auth_group`, `id_perm_module`) VALUES (17, 1, 17);
