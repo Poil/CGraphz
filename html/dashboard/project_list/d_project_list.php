aa129f7cc227049125487a02b6205a57eaa8b10b
Poil <poil@quake.fr>
2014-03-24 20:00:34 +0100

    fix perms on search and better PDO in PERMS class

--- html/dashboard/project_list/d_project_list.php
+++ html/dashboard/project_list/d_project_list.php
@@ -1,6 +1,6 @@
 <?php
 $perm_mod = new PERMS();
-if ($perm_mod->perm_list_module('dashboard',false)) { 
+if ($perm_mod->perm_module('dashboard','view')) { 
 	foreach ($all_project as $project) {
 		if (intval(GET('f_id_config_project'))==$project->id_config_project) {
 			$style=' style="font-weight: bold;" '; 
@@ -11,6 +11,7 @@ if ($perm_mod->perm_list_module('dashboard',false)) {
 		echo '<span><a '.$style.' href="index.php?module=dashboard&amp;component=view&amp;f_id_config_project='.$project->id_config_project.'">'.$project->project_description.'</a></span>';
 	}
 }
+if ($perm_mod->perm_module('dashboard','search')) { 
 ?>
 <div id="f_form_find_server">
 <label for="f_find_server"><?php echo SEARCH ?>:&nbsp;<input type="text" id="f_find_server" name="f_find_server" /></label>
@@ -19,3 +20,4 @@ if ($perm_mod->perm_list_module('dashboard',false)) {
 <script type="text/javascript">
 	jQuery('#f_form_find_server input[name="f_find_server"]').liveSearch({url: '<?php echo DIR_WEBROOT ?>/html/dashboard/project_list/ajax_server_wh_q.php' + '?f_q='});
 </script>
+<?php } ?>
