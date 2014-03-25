a459c97361b5267de552f2510efc54af45f0fa9a
GlennClaranet <glenn.inizan@fr.clara.net>
2014-03-25 14:52:31 +0100

    maj CSS

--- html/menu/menu_project.php
+++ html/menu/menu_project.php
@@ -6,9 +6,8 @@ include(DIR_FSROOT.'/html/dashboard/project_list/d_project_list.php');
 ?>
 </ul>
 <ul>
-	
 <?php
 include(DIR_FSROOT.'/html/dashboard/project_list/r_project_server_list.php');
-include(DIR_FSROOT.'/html/dashboard/project_list/d_project_server_list
-	</select>
+include(DIR_FSROOT.'/html/dashboard/project_list/d_project_server_list.php');
+?>
 </ul>
