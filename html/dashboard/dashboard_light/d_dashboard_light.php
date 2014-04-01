876b9d53020ea6ba6a96a74cd54ec60b77c32fe3
Poil <poil@quake.fr>
2014-04-01 19:23:02 +0200

    Better and Better and Better ... performance on filters scan

--- html/dashboard/dashboard_light/d_dashboard_light.php
+++ html/dashboard/dashboard_light/d_dashboard_light.php
@@ -75,127 +75,117 @@ if (isset($time_start) && isset($time_end)) {
 }
 $dgraph=0;
 if (is_dir($CONFIG['datadir']."/$cur_server->server_name/")) {
+	$myregex='';
 	foreach ($pg_filters as $filter) {
-		$myregex='#^('.$CONFIG['datadir'].'/'.$cur_server->server_name.'/)('.$filter->plugin.')(?:\-('.$filter->plugin_instance.'))?/('.$filter->type.')(?:\-('.$filter->type_instance.'))?\.rrd#';
+		if (empty($myregex)) {
+			$myregex='#^((('.$CONFIG['datadir'].'/'.$cur_server->server_name.'/)('.$filter->plugin.')(?:\-('.$filter->plugin_instance.'))?/('.$filter->type.')(?:\-('.$filter->type_instance.'))?\.rrd)';
+		} else {
+			$myregex=$myregex.'|(('.$CONFIG['datadir'].'/'.$cur_server->server_name.'/)('.$filter->plugin.')(?:\-('.$filter->plugin_instance.'))?/('.$filter->type.')(?:\-('.$filter->type_instance.'))?\.rrd)';
+		}
+	}
+	$myregex=$myregex.')#';
 
-		$plugins = preg_find($myregex, $CONFIG['datadir'].'/'.$cur_server->server_name, PREG_FIND_RECURSIVE|PREG_FIND_FULLPATH|PREG_FIND_SORTBASENAME);
-		if ($plugins) $dgraph=1;
+	$plugins = preg_find($myregex, $CONFIG['datadir'].'/'.$cur_server->server_name, PREG_FIND_RECURSIVE|PREG_FIND_FULLPATH|PREG_FIND_SORTBASENAME);
 
-		$old_t='';
-		$old_pi='';
-		$old_subpg='';
-		foreach ($plugins as $plugin) {
-			preg_match($myregex, $plugin, $matches);
+	if ($plugins) $dgraph=1;
 
-			if (isset($matches[2])) {
-				$p=$matches[2];
-				if (!isset($$p)) $$p=false;
-			} else { 
-				$p=null; 
+	$old_t='';
+	$old_pi='';
+	$old_subpg='';
+	$myregex='#^('.$CONFIG['datadir'].'/'.$cur_server->server_name.'/)(\w+)(?:\-(\w*))?/(\w+)(?:\-(\w*))?\.rrd#';
+	foreach ($plugins as $plugin) {
+		preg_match($myregex, $plugin, $matches);
+
+		if (isset($matches[2])) {
+			$p=$matches[2];
+			if (!isset($$p)) $$p=false;
+		} else { 
+			continue;
+			$p=null; 
+		}
+		if (isset($matches[3])) {
+			$pi=$matches[3];
+			$pc=null;
+			if (substr_count($pi, '-') >= 1 && preg_match($CONFIG['plugin_pcategory'], $p)) {
+				$tmp=explode('-',$pi);
+				// Fix when PI is null after separating PC/PI for example a directory named "MyHost/GenericJMX-cassandra_activity_request-/"
+				if (strlen($tmp[1])) {
+					$pc=$tmp[0];
+					$pi=implode('-', array_slice($tmp,1));
+				}
+			// Copy PI to PC if no PC but Plugin can have a PC
+			} else if (preg_match($CONFIG['plugin_pcategory'], $p)) {
+				$pc=$pi;
+				$pi=null;
 			}
-			if (isset($matches[3])) {
-				$pi=$matches[3];
-				$pc=null;
-				if (substr_count($pi, '-') >= 1 && preg_match($CONFIG['plugin_pcategory'], $p)) {
-					$tmp=explode('-',$pi);
-					// Fix when PI is null after separating PC/PI for example a directory named "MyHost/GenericJMX-cassandra_activity_request-/"
-					if (strlen($tmp[1])) {
-						$pc=$tmp[0];
-						$pi=implode('-', array_slice($tmp,1));
-					}
-				// Copy PI to PC if no PC but Plugin can have a PC
-				} else if (preg_match($CONFIG['plugin_pcategory'], $p)) {
-					$pc=$pi;
-					$pi=null;
-				}
-			} else { 
-				$pc=null; 
-				$pi=null; 
+		} else { 
+			$pc=null; 
+			$pi=null; 
+		}
+		if (isset($matches[4])) {
+			$t=$matches[4];
+		} else { 
+			$t=null; 
+		}
+		if (isset($matches[5])) {
+			$ti=$matches[5];
+			$tc=null;
+			if (substr_count($ti, '-') >= 1 && preg_match($CONFIG['plugin_tcategory'], $p)) {
+				$tmp=explode('-',$ti);
+				$tc=$tmp[0];
+				//$ti=implode('-', array_slice($tmp,1));
+				$ti=null;
+			} else if (preg_match($CONFIG['plugin_tcategory'], $p)) {
+				$tc=$ti;
+				$ti=null;
 			}
-			if (isset($matches[4])) {
-				$t=$matches[4];
-			} else { 
-				$t=null; 
+		} else { 
+			$tc=null; 
+			$ti=null; 
+		}
+
+
+		if (!isset(${$p.$pc.$pi.$t.$tc.$ti}) ) {
+			if ($$p!=true && $p!='aggregation') {
+				$lvl_p=2;
+				$lvl_pc=$lvl_p+1;
+				$lvl_pi=$lvl_pc;
+				$lvl_tc=null;
+				echo "<h$lvl_p>".ucfirst($p)."</h$lvl_p>";
+				$$p=true;
+				$others=false;
+			} else if ($p == 'aggregation') {
+				$lvl_pc=$lvl_p;
+				$lvl_pi=$lvl_pc;
+				$lvl_tc=null;
+				$others=false;
 			}
-			if (isset($matches[5])) {
-				$ti=$matches[5];
-				$tc=null;
-				if (substr_count($ti, '-') >= 1 && preg_match($CONFIG['plugin_tcategory'], $p)) {
-					$tmp=explode('-',$ti);
-					$tc=$tmp[0];
-					//$ti=implode('-', array_slice($tmp,1));
-					$ti=null;
-				} else if (preg_match($CONFIG['plugin_tcategory'], $p)) {
-					$tc=$ti;
-					$ti=null;
-				}
-			} else { 
-				$tc=null; 
-				$ti=null; 
+			// Displaying Plugin Category if there is a Plugin Category
+			if (isset($pc) && empty($$pc)) {
+				echo "<h$lvl_pc>".ucfirst(str_replace('_', ' ', $pc))."</h$lvl_pc>";
+				$lvl_pi=$lvl_pc+1;
+				$$pc=true;
+				$others=false;
+			}
+			// Displaying Plugin Instance for some plugins
+			if (preg_match($CONFIG['title_pinstance'],$p) && strlen($pi) && $$pi!=true) {
+				$$pi=true;
+				echo "<h$lvl_pi>".ucfirst(str_replace('_', ' ',$pi))."</h$lvl_pi>";
 			}
 
-
-			if (!isset(${$p.$pc.$pi.$t.$tc.$ti}) ) {
-				if ($$p!=true && $p!='aggregation') {
-					$lvl_p=2;
-					$lvl_pc=$lvl_p+1;
-					$lvl_pi=$lvl_pc;
-					$lvl_tc=null;
-					echo "<h$lvl_p>".ucfirst($p)."</h$lvl_p>";
-					$$p=true;
-					$others=false;
-				} else if ($p == 'aggregation') {
-					$lvl_pc=$lvl_p;
-					$lvl_pi=$lvl_pc;
-					$lvl_tc=null;
-					$others=false;
-				}
-				// Displaying Plugin Category if there is a Plugin Category
-				if (isset($pc) && empty($$pc)) {
-					echo "<h$lvl_pc>".ucfirst(str_replace('_', ' ', $pc))."</h$lvl_pc>";
-					$lvl_pi=$lvl_pc+1;
-					$$pc=true;
-					$others=false;
-				}
-				// Displaying Plugin Instance for some plugins
-				if (preg_match($CONFIG['title_pinstance'],$p) && strlen($pi) && $$pi!=true) {
-					$$pi=true;
-					echo "<h$lvl_pi>".ucfirst(str_replace('_', ' ',$pi))."</h$lvl_pi>";
-				}
-
-				${$p.$pc.$pi.$t.$tc.$ti}=true;
+			${$p.$pc.$pi.$t.$tc.$ti}=true;
 
 
-				// Verif regex OK
-				if (isset($p) && isset($t)) {
-					if (!preg_match('/^(df|interface|oracle)$/', $p) || 
-					   (((preg_replace('/[^0-9\.]/','',$cur_server->collectd_version) >= 5)
-					     && (preg_replace('/[^a-zA-Z]/','',$cur_server->collectd_version) == 'Collectd') 
-					     && $p!='oracle' && $t!='df'))
-					   || (preg_replace('/[^a-zA-Z]/','',$cur_server->collectd_version) == 'SSC')
-				    ) {
-						$ti='';
-						if ($old_t!=$t or $old_pi!=$pi or $old_pc!=$pc or $old_tc!=$tc)   {
-							if ($CONFIG['graph_type'] == 'canvas') {
-								$_GET['h'] = $cur_server->server_name;
-								$_GET['p'] = $p;
-								$_GET['pc'] = $pc;
-								$_GET['pi'] = $pi;
-								$_GET['t'] = $t;
-								$_GET['tc'] = $tc;
-								$_GET['ti'] = $ti;
-
-								chdir(DIR_FSROOT);
-								include DIR_FSROOT.'/plugin/'.$p.'.php';
-							} else {
-								if ($time_range!='') {
-									echo '<img class="imggraph" '.$zoom.' title="'.DBL_CLICK_ZOOM.'" alt="rrd" src="'.DIR_WEBROOT.'/graph.php?h='.urlencode($cur_server->server_name).'&amp;p='.urlencode($p).'&amp;pc='.urlencode($pc).'&amp;pi='.urlencode($pi).'&amp;t='.urlencode($t).'&amp;tc='.urlencode($tc).'&amp;ti='.urlencode($ti).'&amp;s='.$time_range.'" />'."\n";
-								} else {
-									echo '<img class="imggraph" '.$zoom.' title="'.DBL_CLICK_ZOOM.'" alt="rrd" src="'.DIR_WEBROOT.'/graph.php?h='.urlencode($cur_server->server_name).'&amp;p='.urlencode($p).'&amp;pc='.urlencode($pc).'&amp;pi='.urlencode($pi).'&amp;t='.urlencode($t).'&amp;tc='.urlencode($tc).'&amp;ti='.urlencode($ti).'&amp;s='.$time_start.'&amp;e='.$time_end.'" />'."\n";
-								}
-							}
-						}
-					} else {
+			// Verif regex OK
+			if (isset($p) && isset($t)) {
+				if (!preg_match('/^(df|interface|oracle)$/', $p) || 
+				   (((preg_replace('/[^0-9\.]/','',$cur_server->collectd_version) >= 5)
+				     && (preg_replace('/[^a-zA-Z]/','',$cur_server->collectd_version) == 'Collectd') 
+				     && $p!='oracle' && $t!='df'))
+				   || (preg_replace('/[^a-zA-Z]/','',$cur_server->collectd_version) == 'SSC')
+			    ) {
+					$ti='';
+					if ($old_t!=$t or $old_pi!=$pi or $old_pc!=$pc or $old_tc!=$tc)   {
 						if ($CONFIG['graph_type'] == 'canvas') {
 							$_GET['h'] = $cur_server->server_name;
 							$_GET['p'] = $p;
@@ -215,16 +205,35 @@ if (is_dir($CONFIG['datadir']."/$cur_server->server_name/")) {
 							}
 						}
 					}
-				} else if (DEBUG==true){
-					echo 'ERREUR - p='.$p.' pc='.$pc.' pi='.$pi.' t='.$t.' tc='.$tc.' ti='.$ti.'<br />';
-				} 
+				} else {
+					if ($CONFIG['graph_type'] == 'canvas') {
+						$_GET['h'] = $cur_server->server_name;
+						$_GET['p'] = $p;
+						$_GET['pc'] = $pc;
+						$_GET['pi'] = $pi;
+						$_GET['t'] = $t;
+						$_GET['tc'] = $tc;
+						$_GET['ti'] = $ti;
+
+						chdir(DIR_FSROOT);
+						include DIR_FSROOT.'/plugin/'.$p.'.php';
+					} else {
+						if ($time_range!='') {
+							echo '<img class="imggraph" '.$zoom.' title="'.DBL_CLICK_ZOOM.'" alt="rrd" src="'.DIR_WEBROOT.'/graph.php?h='.urlencode($cur_server->server_name).'&amp;p='.urlencode($p).'&amp;pc='.urlencode($pc).'&amp;pi='.urlencode($pi).'&amp;t='.urlencode($t).'&amp;tc='.urlencode($tc).'&amp;ti='.urlencode($ti).'&amp;s='.$time_range.'" />'."\n";
+						} else {
+							echo '<img class="imggraph" '.$zoom.' title="'.DBL_CLICK_ZOOM.'" alt="rrd" src="'.DIR_WEBROOT.'/graph.php?h='.urlencode($cur_server->server_name).'&amp;p='.urlencode($p).'&amp;pc='.urlencode($pc).'&amp;pi='.urlencode($pi).'&amp;t='.urlencode($t).'&amp;tc='.urlencode($tc).'&amp;ti='.urlencode($ti).'&amp;s='.$time_start.'&amp;e='.$time_end.'" />'."\n";
+						}
+					}
+				}
+			} else if (DEBUG==true){
+				echo 'ERREUR - p='.$p.' pc='.$pc.' pi='.$pi.' t='.$t.' tc='.$tc.' ti='.$ti.'<br />';
 			} 
-			$old_t=$t;
-			$old_tc=$tc;
-			$old_p=$p;
-			$old_pi=$pi;
-			$old_pc=$pc;
-		}
+		} 
+		$old_t=$t;
+		$old_tc=$tc;
+		$old_p=$p;
+		$old_pi=$pi;
+		$old_pc=$pc;
 	}
 }
 
