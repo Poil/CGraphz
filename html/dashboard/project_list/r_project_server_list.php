f57425c45d04254099638743a5ac21afdc38e156
GlennClaranet <glenn.inizan@fr.clara.net>
2014-04-02 10:24:10 +0200

    rectif filtre

--- html/dashboard/project_list/r_project_server_list.php
+++ html/dashboard/project_list/r_project_server_list.php
@@ -6,11 +6,11 @@ if (isset($_GET['f_id_config_project'])) {
 	$f_id_config_role=filter_input(INPUT_GET,'f_id_config_role',FILTER_SANITIZE_NUMBER_INT);
 	$f_id_config_environment=filter_input(INPUT_GET,'f_id_config_environment',FILTER_SANITIZE_NUMBER_INT);
 	
-	if (isset($_GET['f_id_config_environment']) && $f_id_config_environment!==0) {
+	if (isset($_GET['f_id_config_environment']) && $f_id_config_environment!=0) {
 		$JOIN_ENV='LEFT OUTER JOIN config_environment_server ces
 				ON cs.id_config_server=ces.id_config_server';
 		$WHERE_ENV='AND ces.id_config_environment="'.$f_id_config_environment.'"';
-	} else if (isset($_GET['f_id_config_environment']) &&  $f_id_config_environment===0) {
+	} else if (isset($_GET['f_id_config_environment']) &&  $f_id_config_environment==0) {
 		$JOIN_ENV='LEFT OUTER JOIN config_environment_server ces
 				ON cs.id_config_server=ces.id_config_server';
 		$WHERE_ENV='AND ces.id_config_environment IS NULL';
