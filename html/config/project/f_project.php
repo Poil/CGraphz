0515f7d400dfadcdbaf9fd76f67d4b0ad0d8c8cf
Poil <poil@quake.fr>
2014-06-16 23:54:03 +0200

    Hop

--- html/config/project/f_project.php
+++ html/config/project/f_project.php
@@ -1,10 +1,23 @@
-<form name="f_form_project" method="post" action="<?php echo removeqsvar($cur_url, 'f_id_config_project'); ?>">
+<form class="form-horizontal" role="form" name="f_form_project" method="post" action="<?php echo removeqsvar($cur_url, 'f_id_config_project'); ?>">
 	<input type="hidden" name="f_id_config_project" id="f_id_config_project" value="<?php echo @$cur_project->id_config_project; ?>" />
-	<label for="f_project"><?php echo PROJECT ?></label>
-		<input type="text" name="f_project" id="f_project" value="<?php echo @$cur_project->project; ?>" /><br />
-	<label for="f_project_description"><?php echo DESC ?></label>
-		<input type="text" name="f_project_description" id="f_project_description" value="<?php echo @$cur_project->project_description; ?>" /><br />
-	<input type="submit" name="f_submit_project" id="f_submit_project" value="<?php echo SUBMIT ?>" />
+	<div class="form-group">
+		<label class="col-sm-3 control-label" for="f_project"><?php echo PROJECT ?></label>
+		<div class="col-sm-6">
+			<input type="text" name="f_project" id="f_project" value="<?php echo @$cur_project->project; ?>" /><br />
+		</div>
+	</div>
+
+	<div class="form-group">
+	<label class="col-sm-3 control-label" for="f_project_description"><?php echo DESC ?></label>
+		<div class="col-sm-6">
+			<input type="text" name="f_project_description" id="f_project_description" value="<?php echo @$cur_project->project_description; ?>" /><br />
+		</div>
+	</div>
+	<div class="form-group">
+	        <div class="col-sm-offset-3 col-sm-6">
+			<input type="submit" name="f_submit_project" id="f_submit_project" value="<?php echo SUBMIT ?>" />
+		</div>
+	</div>
 </form>
 
 <?php
