4deb18fd7a1176fc8385afe308530099525f1965
Poil <poil@quake.fr>
2014-07-31 22:36:28 +0200

    Forgot the screen_res file :)

--- html/dashboard/zoom/d_zoom.php
+++ html/dashboard/zoom/d_zoom.php
@@ -45,7 +45,7 @@ if (isset($_SESSION['time_end']) && $_SESSION['time_end']!='') {
 if (empty($_SESSION['detail-width'])) $_SESSION['detail-width'] = $CONFIG['detail-width'];
 if (empty($_SESSION['detail-height'])) $_SESSION['detail-height'] = $CONFIG['detail-height'];
 if (empty($_GET['x'])) $_GET['x'] = $_SESSION['detail-width'] - 150;
-if (empty($_GET['y'])) $_GET['y'] = $_SESSION['detail-height'] - 200;
+if (empty($_GET['y'])) $_GET['y'] = $_SESSION['detail-height'] - 350;
 
 chdir(DIR_FSROOT);
 $CONFIG['version']=$cur_server->collectd_version;
