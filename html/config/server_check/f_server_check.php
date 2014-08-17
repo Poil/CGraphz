70664fd74b2f20307c7cd6b80405dfb2934e7b38
Poil <poil@quake.fr>
2014-08-17 15:07:52 +0200

    Fix some notice/warning

--- html/config/server_check/f_server_check.php
+++ html/config/server_check/f_server_check.php
@@ -1,15 +1,18 @@
-<form name="f_form_del_server_check" method="post" action="" onsubmit="return validate_del(this);">
-	<select name="f_server_name_to_del[]" id="f_server_name_to_del" class="multiselect" multiple="multiple">
-		<?php 
-		for ($i=0; $i<$cpt_deleted_server; $i++) {
-			echo '<option value="'.$all_deleted_server[$i]->server_name.'">';
-				echo $all_deleted_server[$i]->server_name;
-			echo '</option>';
-		}
-		?>
-	</select>
-	<div class="clearfix"></div><br />
+<?php
+$sc_form = new Form('horizontal', removeqsvar($cur_url, 'last_action').'&amp;last_action=clean_server');
 
-	<input type="submit" name="f_del_server_check" id="f_del_server_check" value="<?php echo DEL ?>" />
-</form>
+$sc_form->add('select','f_server_name_to_del')
+       ->label(SERVER)
+       ->labelGrid('col-xs-3 col-md-1')
+       ->inputGrid('col-xs-4 col-md-3')
+       ->multiple(true)
+       ->fieldClasses('multiselect')
+       ->options($all_deleted_server, 'server_name', 'server_name');
 
+$sc_form->add('submit', 'f_del_server_check')
+        ->iType('add')
+        ->labelGrid('col-xs-offset-3 col-md-offset-1')
+        ->inputGrid('col-xs-4 col-md-3"')
+        ->value(SUBMIT);
+
+echo $sc_form->bindForm();
