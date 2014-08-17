1e648642e28d1256757090632b337e2c9744bd21
Poil <poil@quake.fr>
2014-08-17 09:43:19 +0200

    Improve group_user form

--- html/auth/group_user/f_group_user.php
+++ html/auth/group_user/f_group_user.php
@@ -1,34 +1,40 @@
 <?php
-$gu_form = new Form('horizontal', removeqsvar($cur_url, 'last_action').'&amp;last_action=edit_user');
-
-$gu_form->add('hidden', 'f_id_auth_group')
-        ->value(@$cur_group->id_auth_group);
-
-$gu_form->add('select','f_id_auth_user')
-        ->options($all_user, 'id_auth_user', 'user')
-        ->label(USER)
-        ->labelGrid('col-xs-3 col-md-3')
-        ->inputGrid('col-xs-9 col-md-9');
-
-$gu_form->add('checkbox','f_manager')
-        ->value('manager')
-        ->label(MANAGER)
-        ->inputGrid('col-sm-offset-3 col-md-9')
-        ->checked(@$cur_group_user->manager);
-
-$gu_form->add('submit', 'f_submit_group_user')
-        ->iType('add')
-        ->labelGrid('col-xs-offset-3 col-md-offset-3')
-        ->inputGrid('col-xs-9 col-md-9')
-        ->value(SUBMIT);
-
-echo $gu_form->bindForm();
-
 if (isset($_GET['f_id_auth_user'])) {
+   /* Edit */
+   $gu_form = new Form('horizontal', removeqsvar($cur_url, 'last_action').'&amp;last_action=edit_user');
+   
+   $gu_form->add('hidden', 'f_id_auth_group')
+           ->value($cur_group->id_auth_group);
+
+   $gu_form->add('hidden', 'f_id_auth_user')
+           ->value($f_id_auth_user);
+   
+   $gu_form->add('text','f_user')
+           ->value($cur_group_user->user)
+           ->readonly(true)
+           ->label(USER)
+           ->labelGrid('col-xs-3 col-md-3')
+           ->inputGrid('col-xs-9 col-md-9');
+   
+   $gu_form->add('checkbox','f_manager')
+           ->value('manager')
+           ->label(MANAGER)
+           ->inputGrid('col-sm-offset-3 col-md-9')
+           ->checked(@$cur_group_user->manager);
+   
+   $gu_form->add('submit', 'f_submit_group_user')
+           ->iType('add')
+           ->labelGrid('col-xs-offset-3 col-md-offset-3')
+           ->inputGrid('col-xs-9 col-md-9')
+           ->value(SUBMIT);
+   
+   echo $gu_form->bindForm();
+
+   /* Delete */
    $gud_form = new Form('horizontal', removeqsvar($cur_url, array('f_id_auth_user','last_action')).'&amp;last_action=edit_user');
 
    $gud_form->add('hidden', 'f_id_auth_user')
-           ->value($cur_user->id_auth_user);
+           ->value($cur_group_user->id_auth_user);
 
    $gud_form->add('hidden', 'f_id_auth_group')
            ->value($f_id_auth_group);
@@ -40,5 +46,31 @@ if (isset($_GET['f_id_auth_user'])) {
            ->value(DEL);
 
    echo $gud_form->bindForm();
+} else {
+   /* Add */
+   $gu_form = new Form('horizontal', removeqsvar($cur_url, 'last_action').'&amp;last_action=edit_user');
+   
+   $gu_form->add('hidden', 'f_id_auth_group')
+           ->value($cur_group->id_auth_group);
+   
+   $gu_form->add('select','f_id_auth_user')
+           ->options($all_user, 'id_auth_user', 'user')
+           ->label(USER)
+           ->labelGrid('col-xs-3 col-md-3')
+           ->inputGrid('col-xs-9 col-md-9');
+   
+   $gu_form->add('checkbox','f_manager')
+           ->value('manager')
+           ->label(MANAGER)
+           ->inputGrid('col-sm-offset-3 col-md-9');
+   
+   $gu_form->add('submit', 'f_submit_group_user')
+           ->iType('add')
+           ->labelGrid('col-xs-offset-3 col-md-offset-3')
+           ->inputGrid('col-xs-9 col-md-9')
+           ->value(SUBMIT);
+   
+   echo $gu_form->bindForm();
+
 }
 ?>
