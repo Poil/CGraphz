ccb0116fd5b4317f448afc23fc4b27a94a07c957
Poil <poil@quake.fr>
2014-08-22 20:14:41 +0200

    Bind Group permissions is now multiselect

--- html/perm/group_module/f_group_module.php
+++ html/perm/group_module/f_group_module.php
@@ -21,7 +21,7 @@ if (isset($_GET['f_id_perm_module'])) {
            ->iType('delete')
            ->value(DEL);
 } else {
-   $gpm_form = new Form('inline', removeqsvar($cur_url, 'last_action').'&amp;last_action=edit_module');
+   $gpm_form = new Form('horizontal', removeqsvar($cur_url, 'last_action').'&amp;last_action=edit_module');
    $gpm_form->fieldset(true);
    $gpm_form->legend(ADD);
 
@@ -29,7 +29,10 @@ if (isset($_GET['f_id_perm_module'])) {
            ->value($cur_group->id_auth_group);
 
    $gpm_form->add('select','f_id_perm_module')
-            ->options($all_module, 'id_perm_module', array('module','component'));
+            ->inputGrid('col-xs-12 col-md-12')
+            ->options($all_module, 'id_perm_module', array('module','component'))
+            ->multiple(true)
+            ->fieldClasses('multiselect');
 
    $gpm_form->add('submit', 'f_submit_group_module')
            ->iType('add')
