31394983cdcfb45f80ba311334e7bb2392818bbd
Poil <poil@quake.fr>
2015-03-30 12:04:10 +0200

    oubli d'ajout de multi path by plugin dans la generation de titre

--- modules/functions.inc.php
+++ modules/functions.inc.php
@@ -188,7 +188,7 @@ function gen_title($h, $p, $pc, $pi, $t, $tc, $ti) {
 		);
 		$rrd_title = str_replace(array_keys($replacements), array_values($replacements), $rrd_title);
 	} else if ($plugin_json[$t]['type']=='iowpm') {
-		$ItemName=file_get_contents($CONFIG['datadir'].'/'.$h.'/'.$p.'-'.$pi.'/ItemName.txt');
+		$ItemName=file_get_contents(getRRDPath($p).'/'.$h.'/'.$p.'-'.$pi.'/ItemName.txt');
 		$rrd_title="$ItemName on $h";
 	}
 	return $rrd_title;
