50a38498604c8fecf048a629caaca967d3b3d8da
Glenn Inizan <glenn.inizan@fr.clara.net>
2015-08-05 17:16:25 +0200

    GIN - rectif bug search

--- modules/claranet/ajax/checkSession.ajax.php
+++ modules/claranet/ajax/checkSession.ajax.php
@@ -1,7 +1,7 @@
 <?php
     // Cette appel ajax permet de savoir si la session est toujours active
 
-
+	include('../../../config/config.php');
     if(!isset($_SESSION)){
         require "../../../modules/claranet/memcacheSessionHandler.class.php";
         session_start();
